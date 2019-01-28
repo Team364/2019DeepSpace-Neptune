3cd52ecdfdaac5c393c5425090064dab2f1bc04b
Keanu Clark <35940194+da1andonly68@users.noreply.github.com>
2019-01-28 16:37:57 -0600

    moved vision commands into folders

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,6 @@
 
 package frc.robot;
 
-import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
