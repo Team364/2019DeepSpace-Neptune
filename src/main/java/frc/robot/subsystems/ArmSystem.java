1a9138b5573b7ab38777d7b600e027d3c802a216
Clinton Brawley <Clinton.Brawley@WINDS.LOCAL>
2019-02-10 13:47:21 -0600

    add absolute encoder funtionality to arm

--- src/main/java/frc/robot/subsystems/ArmSystem.java
+++ src/main/java/frc/robot/subsystems/ArmSystem.java
@@ -5,6 +5,7 @@ import frc.robot.defaultcommands.ArmOpenLoop;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import frc.robot.RobotMap;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 
 public class ArmSystem extends Subsystem {
 
@@ -15,6 +16,10 @@ public class ArmSystem extends Subsystem {
     public ArmSystem() {
         //initialize talons and or victors here
         // arm = new TalonSRX(RobotMap.arm);
+        // arm.configFactoryDefault();
+        // arm.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative,	// Feedback
+        // 0, 											// PID ID
+        // kTimeoutMs);
     }
 
     @Override
@@ -33,8 +38,40 @@ public class ArmSystem extends Subsystem {
         }
    
     }
+    public void ArmClosedLoop(){
+     
+    }
 
     public void stop(){
         //Set the arm to zero here
     }
+      	/**
+	 * @param units CTRE mag encoder sensor units 
+	 * @return degrees rounded to tenths.
+	 */
+	Double ToDeg(int units) {
+		double deg = units * 360.0 / 4096.0;
+
+		/* truncate to 0.1 res */
+		deg *= 10;
+		deg = (int) deg;
+		deg /= 10;
+
+		return deg;
+  }
+  public double getAbsolutePosition() {
+    // /* get the absolute pulse width position */
+    // int pulseWidth = arm.getSensorCollection().getPulseWidthPosition();
+
+    // /**
+    //  * Mask out the bottom 12 bits to normalize to [0,4095],
+    //  * or in other words, to stay within [0,360) degrees 
+    //  */
+    // pulseWidth = pulseWidth & 0xFFF;
+
+    // /* Update Quadrature position */
+    // arm.getSensorCollection().setQuadraturePosition(pulseWidth, kTimeoutMs);    
+    // return ToDeg(arm.getSensorCollection().getQuadraturePosition());
+    return 0;
+}
 }
