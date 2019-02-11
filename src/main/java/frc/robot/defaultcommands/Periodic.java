b473edf0e1e69ca7cca96208f3e34197c7dd66d5
Keanu Clark <35940194+da1andonly68@users.noreply.github.com>
2019-02-11 01:45:48 -0600

    tracks motion of driveTrain in state and sets deadband variables for driveOpenLoop

--- src/main/java/frc/robot/defaultcommands/Periodic.java
+++ src/main/java/frc/robot/defaultcommands/Periodic.java
@@ -28,6 +28,7 @@ public class Periodic extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    //Loop State assignement
     if(States.loopState == States.LoopStates.CLOSED_LOOP){
       ++loops;
       if(loops > 20){
@@ -37,6 +38,19 @@ public class Periodic extends Command {
       }
     }
     }
+    //If a ball is in stow then the action state is ferry
+    if(Robot.superStructure.limitArray[0]){
+      States.actionState = States.ActionStates.FERRY_ACT;
+    }
+    //Drive Train Motion State Assignment
+    double rVel = Robot.superStructure.rightDrive.getVelocity();
+    double lVel = Robot.superStructure.leftDrive.getVelocity();
+    if((Math.abs(rVel) > 0) || (Math.abs(lVel) > 0)){
+      States.driveMotionState = States.DriveMotionStates.MOVING;
+    }else if((rVel == 0)&&(lVel == 0)){
+      States.driveMotionState = States.DriveMotionStates.NOT_MOVING;
+    }
+
   }
   // Make this return true when this Command no longer needs to run execute()
   @Override
