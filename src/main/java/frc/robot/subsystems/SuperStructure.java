452ea08e0d3b2163d5a127c9d650098841e05c1b
Keanu Clark <35940194+da1andonly68@users.noreply.github.com>
2019-02-10 22:09:08 -0600

    adds a talon base object that sets up an initializable talon customized for complex closed loop control

--- src/main/java/frc/robot/subsystems/SuperStructure.java
+++ src/main/java/frc/robot/subsystems/SuperStructure.java
@@ -1,15 +1,12 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.defaultcommands.Periodic;
 import frc.robot.Robot;
+import frc.robot.util.TalonBase;
+import frc.robot.defaultcommands.testOpenLoop;
 
 /**
  * Add your docs here.
@@ -18,14 +15,25 @@ public class SuperStructure extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
   /**tracks whehter or not the lift is in bounds for open loop control */
+  //TODO: Get testSystem to work and then move lift and arm into here -- possibly move drive into here as well
   public boolean liftOutofBounds = false;
   public boolean armOutofBounds = false;
-
+  public TalonBase testSystem;
+  public TalonSRX testTalon;
+  public SuperStructure(){
+    testTalon = new TalonSRX(0);
+    testSystem = new TalonBase(testTalon);
+    testSystem.setDefaultCommand(new testOpenLoop());
+  }
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
      setDefaultCommand(new Periodic());
   }
+  public void runTestTalon(){
+    System.out.println("Test talon position: " + testSystem.getPosition());
+  }
+  
   public void resetEncoders(){
     Robot.liftSystem.zero();
     Robot.armSystem.zero();
