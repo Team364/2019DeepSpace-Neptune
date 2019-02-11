13f681793d28214b848816978155fdbaf82ba717
Keanu Clark <35940194+da1andonly68@users.noreply.github.com>
2019-02-10 22:51:36 -0600

    adds PistonBase

--- src/main/java/frc/robot/subsystems/SuperStructure.java
+++ src/main/java/frc/robot/subsystems/SuperStructure.java
@@ -13,6 +13,10 @@ import frc.robot.defaultcommands.testOpenLoop;
  * Add your docs here.
  */
 public class SuperStructure extends Subsystem {
+  //TLDR: The subsystem code is SUPER repetitve and it wouldn't hurt to reduce the code to
+  //instances of template classes and running methods in here because all the important custom
+  //stuff is run in commands
+  
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
   /**tracks whehter or not the lift is in bounds for open loop control */
@@ -21,9 +25,13 @@ public class SuperStructure extends Subsystem {
   //Really all the specific things are done in commands, so having duplicate subsystem files seem redundant.
   //Would be easier to follow if everything was delegated from a superstructure instead
   //Grip and climb can be there own commands perhaps
+  //Grip system may not need its own system either
+  //Children of superstructure can be required so that other commands cannot access them
   //But arm, lift, and drive are going to be redundant.
   //Auto will mostly be running closed loop stuff with commands
   //Methods aren't really needed and so using TalonBases won't be an issue at all
+  //Theres probably only going to be the superstructure subsystem class and maybe vision
+  //But honestly those methods can be placed in here too
   public boolean liftOutofBounds = false;
   public boolean armOutofBounds = false;
   public TalonBase testSystem;
