70628a3f7ad24e86e3a86fc74c79e652325aa249
Keanu Clark <35940194+da1andonly68@users.noreply.github.com>
2019-03-11 11:06:51 -0500

    organizes Elevate To Position

--- src/main/java/frc/robot/commands/ElevateToPosition.java
+++ src/main/java/frc/robot/commands/ElevateToPosition.java
@@ -40,15 +40,16 @@ public class ElevateToPosition extends Command {
 
     /**
      * Heights
-     * <p>
-     * 1: low on rocket, scoring hatches on rocket level 1 and Cargo Ship
-     * <p>
-     * 2: middle on rocket
-     * <p>
-     * 3: high on rocket
-     * <p>
-     * 4: cargo height for scoring on Cargo Ship
-     * 
+     * <p>0: cargo intake
+     * <p>1: lvl 1 rocket, hatches on Cargo Ship
+     * <p>2: lvl 2 rocket
+     * <p>3: lvl 3 on rocket
+     * <p>4: cargo height for scoring on Cargo Ship
+     * <p>5: inside frame perimeter with game object
+     * <p>6: level 3 climb for deploying forearms
+     * <p>7: return to zero on lift for climbing
+     * <p>8: Unknown
+     * <p>9: level 2 climb for deploying forearms
      * @param Height
      */
     public ElevateToPosition(int Height) {
@@ -67,50 +68,43 @@ public class ElevateToPosition extends Command {
 
     @Override
     protected void execute() {
+        //Sets that are the same across cargo and hatch mode
         liftStartConfig = RobotMap.liftStartConfig;
         frontCam = RobotMap.fCam;
+        intakeCam = RobotMap.l1Ccam;
+        lvlone = RobotMap.armPerpindicularToGround;
+        perpendicularToGround = RobotMap.armPerpindicularToGround;
+        armStartConfig = RobotMap.armStartConfig;
+        cargo = RobotMap.liftCargoC;
+        //Object state dependent sets
         if (States.objState == States.ObjectStates.HATCH_OBJ) {
+            //lift sets
             low = RobotMap.liftLowH;
             med = RobotMap.liftMedH;
             high = RobotMap.liftHighH;
-            cargo = low;
             intake = low;
-
-            perpendicularToGround = RobotMap.armPerpindicularToGround;
-            armStartConfig = RobotMap.armStartConfig;
-            scoreOnHigh = perpendicularToGround;
-            intakeCargo = 3300;
-
-            lvlone = perpendicularToGround;
+            //arm sets
             lvlthree = perpendicularToGround;
-
+            //Camera sets
             l1cam = RobotMap.l1Hcam;
             l2cam = RobotMap.l2Hcam;
-            l3cam = RobotMap.l3Hcam;
-            intakeCam = RobotMap.l1Ccam;
+            l3cam = RobotMap.l3Hcam;  
         } else if (States.objState == States.ObjectStates.CARGO_OBJ) {
+            //lift sets
             intake = RobotMap.liftIntake;
             low = RobotMap.liftLowC;
             med = RobotMap.liftMedC;
             high = RobotMap.liftHighC;
-            cargo = RobotMap.liftCargoC;
-
+            //arm sets
             intakeCargo = RobotMap.armIntakeCargo;
-            perpendicularToGround = RobotMap.armPerpindicularToGround;
-            scoreOnHigh = perpendicularToGround;
-            armStartConfig = RobotMap.armStartConfig;
-            custom = 5000;
-            lvlone = 3300;
-            lvlthree = 2100;
-
+            lvlthree = RobotMap.armScoreOnHigh;
+            //Camera sets
             l1cam = RobotMap.l1Ccam;
             l2cam = RobotMap.l2Ccam;
             l3cam = RobotMap.l3Ccam;
-            intakeCam = RobotMap.l1Ccam;
-
         }
 
-        if (desiredHeight == 0) {
+        if (desiredHeight == 0) {//Intake Position for getting Cargo
             Neptune.elevator.setPlayCruiseVelocity();
             wantedPosition = intake;
             wantedAngle = intakeCargo;
@@ -119,7 +113,7 @@ public class ElevateToPosition extends Command {
                 System.out.println("Intake Position " + States.objState.toString());
             }
             States.led = States.LEDstates.INTAKE_MODE;
-        } else if (desiredHeight == 1) {
+        } else if (desiredHeight == 1) {//Level one on rocket, hatch depo, and cargoship
             Neptune.elevator.setPlayCruiseVelocity();
             wantedPosition = low;
             wantedAngle = lvlone;
@@ -127,7 +121,7 @@ public class ElevateToPosition extends Command {
             if (loops < 1) {
                 System.out.println("Level 1 " + States.objState.toString());
             }
-        } else if (desiredHeight == 2) {
+        } else if (desiredHeight == 2) {//level two on rocket
             Neptune.elevator.setPlayCruiseVelocity();
             wantedPosition = med;
             wantedAngle = perpendicularToGround;
@@ -135,7 +129,7 @@ public class ElevateToPosition extends Command {
             if (loops < 1) {
                 System.out.println("Level 2 " + States.objState.toString());
             }
-        } else if (desiredHeight == 3) {
+        } else if (desiredHeight == 3) {//level 3 on rocket
             Neptune.elevator.setPlayCruiseVelocity();
             wantedPosition = high;
             wantedAngle = lvlthree;
@@ -143,12 +137,15 @@ public class ElevateToPosition extends Command {
             if (loops < 1) {
                 System.out.println("Level 3 " + States.objState.toString());
             }
-        } else if (desiredHeight == 4) {
+        } else if (desiredHeight == 4) {//CargoShip for scoring cargo
             Neptune.elevator.setPlayCruiseVelocity();
             wantedPosition = cargo;
             wantedAngle = 4200;
             camera = frontCam;
-        } else if (desiredHeight == 5) {
+            if (loops < 1) {
+                System.out.println("Scoring Cargo in Cargo Ship");
+            }
+        } else if (desiredHeight == 5) {//Inside frame perimeter with game object
             Neptune.elevator.setPlayCruiseVelocity();
             wantedPosition = liftStartConfig;
             wantedAngle = 300;
@@ -158,34 +155,47 @@ public class ElevateToPosition extends Command {
             }else if(States.objState == States.ObjectStates.HATCH_OBJ){
                 States.led = States.LEDstates.PASSIVE;
             }
-           
-        } else if (desiredHeight == 6) {
+            if (loops < 1) {
+                System.out.println("Collapsing");
+            }
+        } else if (desiredHeight == 6) {//level 3 Climb
             Neptune.elevator.setPlayCruiseVelocity();
             wantedPosition = 87000;
             wantedAngle = lvlone;
             camera = frontCam;
-        } else if (desiredHeight == 7) {
+            if (loops < 1) {
+                System.out.println("Setting up for level 3 climb");
+            }
+        } else if (desiredHeight == 7) {//Return to zero for climbing
             Neptune.elevator.setClimbCruiseVelocity();
             wantedPosition = liftStartConfig;
             wantedAngle = 1500;
             camera = frontCam;
+            if (loops < 1) {
+                System.out.println("Lift is climbing");
+            }
         } else if (desiredHeight == 8) {
             Neptune.elevator.setPlayCruiseVelocity();
             wantedPosition = low + 5000;
             wantedAngle = lvlone;
             camera = l1cam;
-        } else if(desiredHeight == 9){
+        } else if(desiredHeight == 9){//level 2 Climb
             Neptune.elevator.setPlayCruiseVelocity();
             wantedPosition = RobotMap.liftLowC;
             wantedAngle = lvlone;
             camera = frontCam;
+            if (loops < 1) {
+                System.out.println("Setting up for level 2 climb");
+            }
         }
+        //If the position somehow is greater than what is allowed, then
+        //the maximium allowed height is set again
         if (wantedPosition > 132000) {
             wantedPosition = 132000;
         }
         Neptune.elevator.elevateTo(wantedPosition, wantedAngle);
         Neptune.elevator.setCamera(camera);
-        loops++;
+        loops++;//ensures print happen once
     }
 
     @Override
